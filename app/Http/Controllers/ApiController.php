8ef8169038cfd741d0996896c025b785475b7540
= <=>
2021-08-17 19:34:23 +0530

    work on api register and login

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 202);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $responseArr = [];
+        $responseArr['token'] = $user->createToken('myLaunchpad')->accessToken;
+        $responseArr['name'] = $user->name;
+        return response()->json($responseArr, 200);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $responseArr = [];
+            $responseArr['token'] = $user->createToken('myLaunchpad')->accessToken;
+            $responseArr['name'] = $user->name;
+            return response()->json($responseArr, 200);
+        } else {
+            return response()->json(['error' => 'UN AUTHORIZED'], 203);
+        }
+    }
+
+    public function getStudents()
+    {
+        $students = User::where('role_id', 3)->get();
+        $responseArr = ['status' => 'ok', 'data' => $students];
+        return response()->json($responseArr, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [ApiController::class, 'register']);
+Route::post('/login', [ApiController::class, 'login']);
+Route::middleware('auth:api')->get('/students', [ApiController::class, 'getStudents']);
